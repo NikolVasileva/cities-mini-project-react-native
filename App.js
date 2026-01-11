88d8163dfb6c7a5949011946b0e6d4219f937503
NikolVasileva <nikol.vasileva9@gmail.com>
2026-01-11 16:46:10 +0200

    Add logic if there is selected city

--- App.js
+++ App.js
@@ -2,13 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import CityList from './components/CityList.jsx';
 import data from './data.json';
+import { useState } from 'react';
 
 export default function App() {
 
+  const [selectedCity, setSelectedCity] = useState(null)
+
   const cityPressHandler = (cityId) => {
-    alert(cityId)
+    setSelectedCity(cityId)
 }
   return (
+    selectedCity ? alert("test") :
     <CityList cities={data.cities} cityPressHandler={cityPressHandler}/>
   );
 }
